144bc29b9889e383aa6c7fa95c87ce706f92f37b
Dasha Pushkarskaya <d.pushkarskaya.pr@gmail.com>
2020-04-07 22:58:47 +0300

    st

--- script.js
+++ script.js
@@ -28,3 +28,135 @@ module.exports = {
         };
     }
 };
+
+
+for (var i = 0; i < 10; i--) {
+}
+
+for (var i = 10; i >= 0; i++) {
+}
+
+
+for (var i = 0; i < 10; i++) {
+}
+
+
+var p = {
+        get name(){
+            return "nicholas";
+        }
+    };
+
+    Object.defineProperty(p, "age", {
+        get: function (){
+            return 17;
+        }
+    });
+
+
+    p = {
+    get name(){
+        // no returns.
+    }
+};
+
+Object.defineProperty(p, "age", {
+    get: function (){
+        // no returns.
+    }
+});
+
+
+
+class P{
+    get name(){
+        return "nicholas";
+    }
+}
+
+const result = new Promise(function executor(resolve, reject) {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+
+
+const foo = new Promise(async (resolve, reject) => {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+const result = new Promise(async (resolve, reject) => {
+  resolve(await foo);
+});
+
+
+const foo = new Promise((resolve, reject) => {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+const result = Promise.resolve(foo);
+
+
+if (x === -0) {
+    // doSomething()...
+}
+
+
+if (x === 0) {
+    // doSomething()...
+}
+
+
+var x;
+if (x = 0) {
+    var b = 1;
+}
+
+// Practical example that is similar to an error
+function setHeight(someNode) {
+    "use strict";
+    do {
+        someNode.height = "100px";
+    } while (someNode = someNode.parentNode);
+}
+
+
+
+
+var x;
+if (x === 0) {
+    var b = 1;
+}
+
+// Practical example that wraps the assignment in parentheses
+function setHeight(someNode) {
+    "use strict";
+    do {
+        someNode.height = "100px";
+    } while ((someNode = someNode.parentNode));
+}
+
+// Practical example that wraps the assignment and tests for 'null'
+function setHeight(someNode) {
+    "use strict";
+    do {
+        someNode.height = "100px";
+    } while ((someNode = someNode.parentNode) !== null);
+}
